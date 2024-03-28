d0f9bce34d56fb62850bbd5e44cb296d22ea5b33
jjjjjjpppppp <jonphey91@gmail.com>
2024-03-28 13:32:08 +1100

    Created initial list for exercise 1

--- app/components/ExerciseSection.jsx
+++ app/components/ExerciseSection.jsx
@@ -2,13 +2,37 @@
 import React, { useTransition, useState } from 'react'
 import Image from 'next/image'
 import TabButton from './TabButton'
+import Link from 'next/link'
 
 const TAB_DATA = [
     {
         title: "Exercise 1",
         id: "exercise1",
         content: ( 
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis aspernatur voluptate distinctio temporibus error sunt nisi dolorem laudantium, aliquid non, nobis tempore molestiae quas, rem provident obcaecati expedita quod recusandae odio. Quidem commodi harum, ea libero sequi necessitatibus reiciendis temporibus velit facere neque atque quae illo minima voluptate, fugit quasi debitis iste aspernatur! Nihil repudiandae nobis voluptas, dolor illum, eligendi commodi labore maiores ad iure eos blanditiis similique et excepturi facilis, architecto deleniti in suscipit minus accusamus sit totam. Provident sint assumenda iure! Excepturi magnam earum sit, repellendus a natus animi repellat consectetur dolor ea nesciunt alias accusamus nulla nobis iusto commodi mollitia! Porro perferendis totam perspiciatis, aperiam cupiditate veniam sit ullam mollitia id quam rerum qui tempora voluptatum voluptas temporibus ducimus. Tempora eveniet eius dolores error quos quo repellendus, deleniti deserunt rerum quia voluptas optio dolorum necessitatibus sit cupiditate, architecto dicta accusantium unde ab, et eum veritatis ullam! Neque hic magnam corrupti dolor voluptatum quisquam enim deserunt adipisci quis quas quasi odit perspiciatis, delectus ad facere debitis quos amet recusandae fuga et minus placeat. Deserunt dolore aut temporibus omnis ab rem, error iure perspiciatis porro labore iste eligendi id consectetur! Numquam accusamus vel repellat vero, voluptatibus quidem deserunt minima.</p>
+            <>
+            <p className='italic'>From <Link href='https://gist.github.com/Pieparker/b04a4e9ff82ba949e5db9d5b0e9d89e8' className=' text-pink-400' target="_blank">this</Link> list, rank your 5 most favourite and 5 least favourite support tasks. Provide a brief explanation for each.</p>
+                <br></br>
+                <p className='font-bold'>Favourites</p>
+                    <ol className=' list-decimal pl-5'>
+                        <li>Run ticket review sessions to make sure tone is consistent</li>
+                            <ul className=' list-disc pl-5'>
+                                <li>Reason</li>
+                            </ul>
+                        <li>Work with the product team to develop a new feature based on feedback from customers</li>
+                        <li>Find and recruit teammates for the support team</li>
+                        <li>Manage a support team</li>
+                        <li>Write and maintain support articles and docs pages</li>
+                    </ol>
+                <br></br>
+                <p className='font-bold'>Least favourite</p>
+                    <ol className=' list-decimal pl-5'>
+                        <li>Help resolve billing issues for customers</li>
+                        <li>Work with 3rd party partners to track down a tricky situation for a joint customer</li>
+                        <li>Analyze hundreds of support tickets to spot trends the product team can use</li>
+                        <li>Engage multiple users at once in a public discussion, to answer their questions and troubleshoot problems</li>
+                        <li>Identify, file (and, where possible, resolve) bugs in private and public Vercel/Next.js repos on GitHub</li>
+                    </ol>
+            </>
         )
     },
     {
